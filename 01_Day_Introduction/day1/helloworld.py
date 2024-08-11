08130a5a05fb3ff18aaa503c8315affab55885dc
rekaz <rekaz.muddassir@gmail.com>
2024-08-12 05:21:43 +0530

    day 1 and day2

---
+++ 01_Day_Introduction/day1/helloworld.py
@@ -0,0 +1,40 @@
+# print (python --version)
+print (3+2)
+print (3-2)
+print (3*2)
+print (3%2)
+print (3/2)
+print (3**2)
+print (3//2)
+print ("Rekaz")
+print ("Nahid and Muddassir Family")
+print ("India")
+print ("I am enjoying 30 days of Python")
+print (type(2))
+print (type (9.8))
+print (type (3.14))
+print (type (4-4j))
+print (type (['Asabeneh', 'Python', 'Finland']))
+print (type ("Rekaz"))
+print (type ("Nahid and Muddassir Family"))
+print (type ("India"))
+
+#example of list
+print (type([1,3, 4, 4, 5]))
+
+#example of boolean 
+print (type (True))
+
+#example of Tuple
+print (type ((2,3)))
+
+#example of Set
+print (type ({2,3,4,4,4}))
+
+#example of dictionary
+print (type ({"key":2}))
+
+print (((2-10)**2 + (3-8)**2)**(1/2))
+
+       
+
---
+++ 02_Day_Variables_builtin_functions/day2/variables.py
@@ -0,0 +1,54 @@
+#Day 2: 30 days of python programming
+first_Name = "John"
+last_Name = "Wick"
+full_Name = "John Wick"
+country = "India"
+city = "New Delhi"
+age = 32
+year = 2024
+is_married = True
+is_True = True
+is_light_on = True
+a ,b ,c = 1,1.23, "mm"
+
+print (type(first_Name))
+print (type(last_Name))
+print (type(full_Name))
+print (type(country))
+print (type(city))
+print (type(age))
+print (type(year))
+print (type(is_married))
+print (type(is_True))
+print (type(is_light_on))
+print (type(a))
+print (type(b))
+print (type(c))
+
+print (len (first_Name))
+print (len (last_Name))
+
+num1 = 5
+num2 = 4
+
+total = num1 + num2
+diff = num1 - num2
+product = num1 * num2
+division = num1 / num2
+remainder = num1 % num2
+exp = num1 ** num2
+print (exp)
+floor_division = num1//num2
+
+r = 30
+area_of_circle = 3.14 * float(r**2)
+circum_of_circle = 2 *3.14* r
+r_input = int(input ("Enter r:"))
+area = 3.14 * float (r_input ** 2)
+
+first_Name = input ("First Name: ")
+last_Name = input ("Last Name: ") 
+country = input ("Country: ") 
+age = input ("Age: ") 
+
+print (help('keywords'))
